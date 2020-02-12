b8f3e8e7900b107fd7f5d22a671500b5b35b9b21
Igor Santos <igorcost10@gmail.com>
2020-02-12 20:39:59 -0300

    Primeiras alterações no modelo de api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import fs from "fs";
+
 const app = express();
 const porta = 8080;
 
+fs.readFile("./data.json", "utf8", (err, data) => {
+    if(err){
+        // tslint:disable-next-line:no-console
+        return console.log("Erro ao ler arquivo");
+    }
+
+    const jsonData = JSON.parse(data);
+  });
+
 app.get( "/", ( req, res ) => {
-    res.send( "Oi! ^^" );
+    res.status(200).send({
+        title: "TS-CLINICA-WEBAPI",
+        version: "1.0.0"
+    });
+    res.status(400).send( "Oi! Houve um erro ^^" );
 } );
 
 app.listen( porta, () => {
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,29 +1,21 @@
 import * as express from "express";
 
 export const register = ( app: express.Application ) => {
-    const oidc = app.locals.oidc;
-    const port = parseInt( process.env.PGPORT || "5432", 10 );
-    const config = {
-        database: process.env.PGDATABASE || "postgres",
-        host: process.env.PGHOST || "localhost",
-        port,
-        user: process.env.PGUSER || "postgres"
-    };
 
-    app.get( `/api/rules/all`, oidc.ensureAuthenticated(), async ( req: any, res ) => {
+    app.get( `/api/rules/all`, async ( req: any, res ) => {
         try {
-            const userId = req.userContext.userinfo.sub;
-            const rules = await db.any( `
-                SELECT
-                    id
-                    , brand
-                    , model
-                    , year
-                    , color
-                FROM    rules
-                WHERE   user_id = $[userId]
-                ORDER BY year, brand, model`, { userId } );
-            return res.json( rules );
+            // const userId = req.userContext.userinfo.sub;
+            // const rules = await db.any( `
+            //     SELECT
+            //         id
+            //         , brand
+            //         , model
+            //         , year
+            //         , color
+            //     FROM    rules
+            //     WHERE   user_id = $[userId]
+            //     ORDER BY year, brand, model`, { userId } );
+            // return res.json( rules );
         } catch ( err ) {
             // tslint:disable-next-line:no-console
             console.error(err);
@@ -31,18 +23,18 @@ export const register = ( app: express.Application ) => {
         }
     } );
 
-    app.get( `/api/rules/total`, oidc.ensureAuthenticated(), async ( req: any, res ) => {
+    app.get( `/api/rules/all`, async ( req: any, res ) => {
         try {
-            const userId = req.userContext.userinfo.sub;
-            const total = await db.one( `
-            SELECT  count(*) AS total
-            FROM    rules
-            WHERE   user_id = $[userId]`, { userId }, ( data: { total: number } ) => {
-                return {
-                    total: +data.total
-                };
-            } );
-            return res.json( total );
+            // const userId = req.userContext.userinfo.sub;
+            // const total = await db.one( `
+            // SELECT  count(*) AS total
+            // FROM    rules
+            // WHERE   user_id = $[userId]`, { userId }, ( data: { total: number } ) => {
+            //     return {
+            //         total: +data.total
+            //     };
+            // } );
+            return res.json( { text: "all" } );
         } catch ( err ) {
             // tslint:disable-next-line:no-console
             console.error(err);
@@ -50,21 +42,21 @@ export const register = ( app: express.Application ) => {
         }
     } );
 
-    app.get( `/api/rules/find/:search`, oidc.ensureAuthenticated(), async ( req: any, res ) => {
+    app.get( `/api/rules/find/:search`, async ( req: any, res ) => {
         try {
-            const userId = req.userContext.userinfo.sub;
-            const rules = await db.any( `
-                SELECT
-                    id
-                    , brand
-                    , model
-                    , year
-                    , color
-                FROM    rules
-                WHERE   user_id = $[userId]
-                AND   ( brand ILIKE $[search] OR model ILIKE $[search] )`,
-                { userId, search: `%${ req.params.search }%` } );
-            return res.json( rules );
+            // const userId = req.userContext.userinfo.sub;
+            // const rules = await db.any( `
+            //     SELECT
+            //         id
+            //         , brand
+            //         , model
+            //         , year
+            //         , color
+            //     FROM    rules
+            //     WHERE   user_id = $[userId]
+            //     AND   ( brand ILIKE $[search] OR model ILIKE $[search] )`,
+            //     { userId, search: `%${ req.params.search }%` } );
+            return res.json( { text: "search" } );
         } catch ( err ) {
             // tslint:disable-next-line:no-console
             console.error(err);
@@ -72,15 +64,15 @@ export const register = ( app: express.Application ) => {
         }
     } );
 
-    app.post( `/api/rules/add`, oidc.ensureAuthenticated(), async ( req: any, res ) => {
+    app.post( `/api/rules/add`, async ( req: any, res ) => {
         try {
-            const userId = req.userContext.userinfo.sub;
-            const id = await db.one( `
-                INSERT INTO rules( user_id, brand, model, year, color )
-                VALUES( $[userId], $[brand], $[model], $[year], $[color] )
-                RETURNING id;`,
-                { userId, ...req.body  } );
-            return res.json( { id } );
+            // const userId = req.userContext.userinfo.sub;
+            // const id = await db.one( `
+            //     INSERT INTO rules( user_id, brand, model, year, color )
+            //     VALUES( $[userId], $[brand], $[model], $[year], $[color] )
+            //     RETURNING id;`,
+            //     { userId, ...req.body  } );
+            return res.json( { text: "add" } );
         } catch ( err ) {
             // tslint:disable-next-line:no-console
             console.error(err);
@@ -88,22 +80,22 @@ export const register = ( app: express.Application ) => {
         }
     } );
 
-    app.post( `/api/rules/update`, oidc.ensureAuthenticated(), async ( req: any, res ) => {
+    app.post( `/api/rules/update`, async ( req: any, res ) => {
         try {
-            const userId = req.userContext.userinfo.sub;
-            const id = await db.one( `
-                UPDATE rules
-                SET brand = $[brand]
-                    , model = $[model]
-                    , year = $[year]
-                    , color = $[color]
-                WHERE
-                    id = $[id]
-                    AND user_id = $[userId]
-                RETURNING
-                    id;`,
-                { userId, ...req.body  } );
-            return res.json( { id } );
+            // const userId = req.userContext.userinfo.sub;
+            // const id = await db.one( `
+            //     UPDATE rules
+            //     SET brand = $[brand]
+            //         , model = $[model]
+            //         , year = $[year]
+            //         , color = $[color]
+            //     WHERE
+            //         id = $[id]
+            //         AND user_id = $[userId]
+            //     RETURNING
+            //         id;`,
+            //     { userId, ...req.body  } );
+            return res.json( { text: "update" } );
         } catch ( err ) {
             // tslint:disable-next-line:no-console
             console.error(err);
@@ -111,16 +103,16 @@ export const register = ( app: express.Application ) => {
         }
     } );
 
-    app.delete( `/api/rules/remove/:id`, oidc.ensureAuthenticated(), async ( req: any, res ) => {
+    app.delete( `/api/rules/remove/:id`, async ( req: any, res ) => {
         try {
-            const userId = req.userContext.userinfo.sub;
-            const id = await db.result( `
-                DELETE
-                FROM    rules
-                WHERE   user_id = $[userId]
-                AND     id = $[id]`,
-                { userId, id: req.params.id  }, ( r ) => r.rowCount );
-            return res.json( { id } );
+            // const userId = req.userContext.userinfo.sub;
+            // const id = await db.result( `
+            //     DELETE
+            //     FROM    rules
+            //     WHERE   user_id = $[userId]
+            //     AND     id = $[id]`,
+            //     { userId, id: req.params.id  }, ( r ) => r.rowCount );
+            // return res.json( { id } );
         } catch ( err ) {
             // tslint:disable-next-line:no-console
             console.error(err);
