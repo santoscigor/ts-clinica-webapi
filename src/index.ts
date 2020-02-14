dfdb6e787d34bcc04472c890dcf2b6cb8c8a33f0
Igor Santos <igorcost10@gmail.com>
2020-02-14 19:51:23 -0300

    alterações no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,16 @@
 import express from "express";
-import fs from "fs";
 
+import shiftRoutes from './routes/shift';
+import {db} from './services/db';
+
+db.checkDb();
 const app = express();
-const porta = 8080;
+const port = process.env.PORT || 8080;
 
-fs.readFile("./data.json", "utf8", (err, data) => {
-    if(err){
-        // tslint:disable-next-line:no-console
-        return console.log("Erro ao ler arquivo!");
-    }
+app.use(express.json());
 
-    const jsonData = JSON.parse(data);
-  });
+app.use("/shifts", shiftRoutes);
 
-app.get( "/", ( req, res ) => {
-    res.status(200).send({
-        title: "TS-CLINICA-WEBAPI",
-        version: "1.0.0"
-    });
-    res.status(400).send( "Oi! Houve um erro com a API ^^'" );
-} );
-
-app.listen( porta, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `servidor iniciado em: http://localhost:${ porta }` );
-} );
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
